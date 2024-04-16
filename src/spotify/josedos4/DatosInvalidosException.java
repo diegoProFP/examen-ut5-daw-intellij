41ef9142bf8a82f234d765e5e2ebfc33eb1fda6c
CrankyJJ <dosreisj445@gmail.com>
2024-04-16 09:57:42 +0200

    creacion de la rama jose, añadimos las clases de spotify al mi paquete

---
+++ src/spotify/josedos4/DatosInvalidosException.java
@@ -0,0 +1,10 @@
+package spotify.josedos4;
+
+public class DatosInvalidosException extends Exception {
+
+	public DatosInvalidosException(String mensaje) {
+		super(mensaje);
+	}
+	
+
+}
---
+++ src/spotify/josedos4/OperacionNoPermitidaException.java
@@ -0,0 +1,10 @@
+package spotify.josedos4;
+
+public class OperacionNoPermitidaException extends Exception {
+
+	public OperacionNoPermitidaException(String mensaje) {
+		super(mensaje);
+	}
+	
+
+}
